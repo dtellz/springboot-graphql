61e8c76bf625d06cf6d64d643568376918afea33
Diego <dtellez95@gmail.com>
2024-02-10 20:38:23 +0000

    add book controller

---
+++ src/main/java/com/example/springbootgraphql/controllers/BookController.java
@@ -0,0 +1,17 @@
+package com.example.springbootgraphql.controllers;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+import com.example.springbootgraphql.data.*;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) { return Book.getById(id);};
+
+    @SchemaMapping
+    public Author author(Book book) {return Author.getById(book.authorId());};
+
+}
